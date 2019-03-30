a5772c013b7881603644f5bd17c3073c2919bf43
Mohan Ram <mohanramphp@gmail.com>
2019-03-30 14:17:23 +0530

    functionality for the game is added

--- projects/tic-tac-toe/src/app/game/board/board.component.ts
+++ projects/tic-tac-toe/src/app/game/board/board.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { BOARD_TOKEN } from './config/board.token';
 import { board } from './config/board.data';
+
+
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
@@ -14,12 +16,62 @@ import { board } from './config/board.data';
 })
 export class BoardComponent implements OnInit {
 
+  squares: Array<'X' | 'O' | null> = [];
+  xIsNext: boolean;
+  winner: 'X' | 'O' | null;
+
   constructor(
     @Inject(BOARD_TOKEN) public rows: Array<Array<number>>
-  ) { }
+  ) {
+    this.defaultBoardSetup();
+  }
 
   ngOnInit() {
 
+  }
+
+  handleSquareSelection(i: number) {
+    const squares = this.squares.slice();
+    squares[i] = this.xIsNext ? 'X' : 'O';
+    this.squares = squares;
+    this.xIsNext = !this.xIsNext;
+    this.winner = this.calculateWinner(this.squares);
+    console.log('this.winner');
+    console.log(this.winner);
+  }
+
+  reset() {
+    this.defaultBoardSetup();
+  }
+
+  defaultBoardSetup() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  calculateWinner(squares): null | 'X' | 'O' {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    let winner = null;
+    lines.forEach((_, i) => {
+      const [a, b, c] = lines[i];
+      console.log('squares[a], squares[b], squares[c]');
+      console.log(squares[a], squares[b], squares[c]);
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        console.log('result = ' + squares[a]);
+        winner = squares[a];
+      }
+    });
+    return winner;
   }
 
 }
--- projects/tic-tac-toe/src/app/game/board/square/square.component.ts
+++ projects/tic-tac-toe/src/app/game/board/square/square.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-square',
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SquareComponent implements OnInit {
 
   @Input() value: number;
+  @Input() disabled: boolean;
+  @Output() squareSelection: EventEmitter<void> = new EventEmitter<void>();
 
   constructor() { }
 
